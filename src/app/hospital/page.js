b9e721d5f4bb7b6b0b7a4c0ddd84bd4ae32a8ea6
boy-brahmanda <boybrahmanda03@gmail.com>
2024-05-09 00:58:08 +0800

    adding card and fixing fetch data

--- src/app/hospital/page.js
+++ src/app/hospital/page.js
@@ -1,29 +1,36 @@
 import Navbar from "../navbar";
 import Image from "next/image";
 
-async function getData() {
-  const url = "http://gis_2105551149.local.net/api";
-  let data;
-  try {
-    const response = await fetch(url + "/hospital");
-    if (!response.ok) {
-      throw new Error("Terjadi kesalahan saat memuat data");
-    }
-    data = await response.json();
-    console.log(data.data);
-  } catch (error) {
-    console.error("Terjadi kesalahan:", error);
-  }
-  return data;
-}
-
 export default function Hospital() {
-  const hospital = getData();
-  console.log(hospital);
+  let datas = null;
+  fetch(url + "/hospital")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      datas = data;
+      console.log(data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   return (
     <>
       <Navbar />
       <h1 className="px-24 p-8 font-bold text-3xl">Hospital List</h1>
+      <div className="grid grid-cols-4 mx-auto px-24 gap-5">
+        {datas.map((data) => (
+          <div key={data.id}>
+            <div className="h-fit grid grid-cols-1 p-8 shadow-md rounded-lg border border-gray-200 hover:shadow-lg">
+              <Image src={bimc} height={200} alt="bimc" />
+              <p className="mt-5 mb-3 text-xl font-semibold border border-black">{data.name}</p>
+              <p>{data.alamat}</p>
+              <p>{data.lat}</p>
+              <p>{data.lng}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }

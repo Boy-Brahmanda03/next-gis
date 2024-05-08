e1cedf54d8b6dfb64849364239c47c9ef02145a6
boy-brahmanda <boybrahmanda03@gmail.com>
2024-05-09 01:14:30 +0800

    edit use effect

--- src/app/hospital/page.js
+++ src/app/hospital/page.js
@@ -21,13 +21,14 @@ export default function Hospital() {
       .catch((error) => {
         console.log(error);
       });
-  });
+  }, [])
+  ;
   return (
     <>
       <Navbar />
       <h1 className="px-24 p-8 font-bold text-3xl">Hospital List</h1>
       <div className="grid grid-cols-4 mx-auto px-24 gap-5">
-        {data.map((data) => (
+        {data && data.map((data) => (
           <div key={data.id}>
             <div className="h-fit grid grid-cols-1 p-8 shadow-md rounded-lg border border-gray-200 hover:shadow-lg">
               <Image src={bimc} height={200} alt="bimc" />

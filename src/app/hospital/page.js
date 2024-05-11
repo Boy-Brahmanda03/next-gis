771b96497317953a2acbbdeca9388e9e5fddfec7
boy-brahmanda <boybrahmanda03@gmail.com>
2024-05-12 02:01:15 +0800

    editing get data on hospitals

--- src/app/hospital/page.js
+++ src/app/hospital/page.js
@@ -1,42 +1,52 @@
+"use client";
+
 import Navbar from "../navbar";
 import Card from "@/app/hospital/card";
 import Button from "./button";
+import { useEffect, useState } from "react";
 
-async function getData() {
-  try {
-    const url = process.env.NEXT_SERVER_PUBLIC_API_URL;
-    const res = await fetch(url + "/hospital", {
+export default function Hospital() {
+  const [token, setToken] = useState("");
+  const [hospitals, setHospitals] = useState();
+
+  useEffect(() => {
+    const tokenFromStorage = localStorage.getItem("token");
+    setToken(tokenFromStorage);
+  }, []);
+
+  console.log(token);
+  useEffect(() => {
+    fetch("http://localhost:8000/api/hospital", {
       method: "GET",
       headers: {
-        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+        "Content-Type": "application/x-www-form-urlencoded",
       },
       cache: "no-store",
-    });
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setHospitals(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, [token]);
 
-    if (!res.ok) {
-      throw new Error("Error fetching data!");
-    }
-    return res.json();
-  } catch (error) {
-    console.error("Fetch error:", error);
-  }
-}
-
-export default async function Hospital() {
-  const hospitals = await getData();
   console.log(hospitals);
   return (
     <>
-      <Navbar />
+      <Navbar token={token} />
       <div className="flex flex-wrap">
         <h1 className="flex-1 px-24 p-8 font-bold text-3xl">Hospital List</h1>
         <div className="px-24 p-8">
           <Button className="w-20 h-11 shadow-sm rounded-lg border border-gray-200 hover:bg-gray-50 hover:shadow-2xl" />
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 px-24 py-4">
-        <Card data={hospitals} />
-      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 px-24 py-4">{hospitals && hospitals.success == true ? <Card data={hospitals} /> : <p>Salah cuy</p>}</div>
     </>
   );
 }

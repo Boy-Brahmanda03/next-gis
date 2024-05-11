85024a0c6b124a2d5a6c399b473060de04e44b0e
boy-brahmanda <boybrahmanda03@gmail.com>
2024-05-11 15:45:14 +0800

    adding add button and close icon

--- src/app/hospital/page.js
+++ src/app/hospital/page.js
@@ -1,5 +1,6 @@
 import Navbar from "../navbar";
 import Card from "@/app/hospital/card";
+import Button from "./button";
 
 async function getData() {
   try {
@@ -27,7 +28,12 @@ export default async function Hospital() {
   return (
     <>
       <Navbar />
-      <h1 className="px-24 p-8 font-bold text-3xl">Hospital List</h1>
+      <div className="flex flex-wrap">
+        <h1 className="flex-1 px-24 p-8 font-bold text-3xl">Hospital List</h1>
+        <div className="px-24 p-8">
+          <Button className="w-20 h-11 shadow-sm rounded-lg border border-gray-200 hover:bg-gray-50 hover:shadow-2xl" />
+        </div>
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 px-24 py-4">
         <Card data={hospitals} />
       </div>
